e1ef0c38197fa49c2b60a0d2c09b96f057a034e6
johnathanDOS <jsonnen89@gmail.com>
2017-11-20 19:25:44 -0700

    Create book detail view

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -42,9 +42,29 @@ exports.book_list = function(req, res, next) {
     };
 
 // Display detail page for a specific book
-exports.book_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+exports.book_detail = function(req, res, next) {
+    
+    async.parallel({
+        book: function(callback) {
+
+            Book.findById(req.params.id)
+                .populate('author')
+                .populate('genre')
+                .exec(callback);
+        },
+        book_instance: function(callback) {
+            
+            BookInstance.find({ 'book': req.params.id })
+            //.populate('book')
+            .exec(callback);
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        // Succesful, so render
+        res.render('book_detail', { title: 'Title', book: results.book, book_instances: results.book_instance });
+    });
 };
+
 
 // Display book create form on GET
 exports.book_create_get = function(req, res) {
