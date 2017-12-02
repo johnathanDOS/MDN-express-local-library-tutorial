bb530cfad80e2ce259cd3fb9a46a951d788b7c99
johnathanDOS <jsonnen89@gmail.com>
2017-12-02 08:42:47 -0700

    Implement GET controls for genre delete view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -98,8 +98,27 @@ exports.genre_create_post = function(req, res, next) {
 };
 
 // Display Genre delete form on GET
-exports.genre_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete GET');
+exports.genre_delete_get = function(req, res, next) {
+
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id)
+                .exec(callback);
+        },
+
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id })
+                .exec(callback);
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        // Success, so render
+        res.render('genre_delete', {
+            title: "Delete Genre",
+            genre: results.genre,
+            genre_books: results.genre_books
+        });
+    });
 };
 
 // Handle Genre delete on POST
